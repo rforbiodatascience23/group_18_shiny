5c5573d0b8214f983ed1e002c455655a56ce01f6
novadtu <s222373@dtu.dk>
2023-10-31 10:31:05 +0100

    Module name changed

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -20,8 +20,8 @@ attachment::att_amend_desc()
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module(name = "module1", with_test = FALSE) # Name of the module
-golem::add_module(name = "module2", with_test = FALSE) # Name of the module
+golem::add_module(name = "random_sequence", with_test = FALSE) # Name of the module
+golem::add_module(name = "plot_frequency", with_test = FALSE) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
